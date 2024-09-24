f32124b03efac4817333ee63f1bb77f6e40110ac
SantiDevs <santi.dev.art@gmail.com>
2024-09-23 19:23:36 -0500

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@nestjs/common';
+
+/**
+ *
+ */
+@Injectable()
+export class AppService {
+  // eslint-disable-next-line jsdoc/require-returns
+  /**
+   *
+   */
+  getHello(): string {
+    return 'Hello World!';
+  }
+}
